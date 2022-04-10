6469105eee62b4ffc9afa7ebfd11f9c0493cf5bf
aldotele <aldo.telese@hotmail.it>
2022-04-10 14:52:34 +0200

    first commit

---
+++ src/main/java/com/world/worldproxy/WorldproxyApplication.java
@@ -0,0 +1,13 @@
+package com.world.worldproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldproxyApplication.class, args);
+	}
+
+}
