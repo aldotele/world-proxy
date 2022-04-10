c1d3785abe4fd0b9ba159149c139cf1916afbdbc
aldotele <aldo.telese@hotmail.it>
2022-04-10 14:56:14 +0200

    changed applcation name

--- src/main/java/com/world/worldproxy/WorldProxyApplication.java
+++ src/main/java/com/world/worldproxy/WorldProxyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorldproxyApplication {
+public class WorldProxyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorldproxyApplication.class, args);
+		SpringApplication.run(WorldProxyApplication.class, args);
 	}
 
 }
