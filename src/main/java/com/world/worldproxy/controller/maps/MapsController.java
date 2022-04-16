e4f673054b91270a45746745225b7fab818dbfa8
aldotele <aldo.telese@hotmail.it>
2022-04-17 01:17:40 +0200

    adds bean in config, adds new service

---
+++ src/main/java/com/world/worldproxy/controller/maps/MapsController.java
@@ -0,0 +1,27 @@
+package com.world.worldproxy.controller.maps;
+
+import com.world.worldproxy.service.CountryMapService;
+import com.world.worldproxy.service.CountrySummaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping(path = "maps")
+class MapsController {
+
+    @Autowired
+    private final CountryMapService countryMapService;
+
+    MapsController(CountryMapService countryMapService) {
+        this.countryMapService = countryMapService;
+    }
+
+    @GetMapping("/{country}")
+    String getAllCountries(@PathVariable String country) {
+        return countryMapService.getMapsByCountryName(country);
+    }
+
+}
---
+++ src/main/java/com/world/worldproxy/service/CountryMapService.java
@@ -0,0 +1,24 @@
+package com.world.worldproxy.service;
+
+import com.world.worldproxy.model.CountrySummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class CountryMapService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${restcountries.base.url}")
+    String restCountriesBaseUrl;
+
+    public String getMapsByCountryName(String country) {
+        ResponseEntity<String> response = restTemplate.getForEntity(restCountriesBaseUrl + "/name/" + country, String.class);
+        return response.getBody();
+    }
+}
