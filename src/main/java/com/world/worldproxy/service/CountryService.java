4e1f06c8459835071a9ac475e5edb451ebfaf9e0
aldotele <aldo.telese@hotmail.it>
2022-04-18 13:46:36 +0200

    adds country interface

--- src/main/java/com/world/worldproxy/service/CountryService.java
+++ src/main/java/com/world/worldproxy/service/CountryService.java
@@ -1,43 +1,12 @@
 package com.world.worldproxy.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.world.worldproxy.model.Country;
-import org.json.JSONArray;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 
-
-@Service
-public class CountryService {
-
-    @Autowired
-    RestTemplate restTemplate;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @Value("${restcountries.base.url}")
-    String restCountriesBaseUrl;
-
-    public List<Country> getAllCountriesRawData() throws JsonProcessingException {
-        ResponseEntity<String> response = restTemplate.getForEntity(restCountriesBaseUrl + "/all", String.class);
-        List<Country> countries = Arrays.asList(objectMapper.readValue(response.getBody(), Country[].class));
-        return countries;
-    }
-
-    public Country getCountryByName(String name) throws JsonProcessingException {
-        ResponseEntity<String> response = restTemplate.getForEntity(restCountriesBaseUrl + "/name/" + name, String.class);
-        JSONArray jsonArray = new JSONArray(response.getBody());
-        Country country = objectMapper.readValue(jsonArray.get(0).toString(), Country.class);
-        return country;
-    }
-
+public interface CountryService {
+    List<Country> getAllCountries() throws JsonProcessingException;
+    Country getCountryByName(String name) throws JsonProcessingException;
+    String getMapsByCountryName(String country);
 }
---
+++ src/main/java/com/world/worldproxy/service/CountryServiceImpl.java
@@ -0,0 +1,50 @@
+package com.world.worldproxy.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.world.worldproxy.model.Country;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Service
+public class CountryServiceImpl implements CountryService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Value("${restcountries.base.url}")
+    String restCountriesBaseUrl;
+
+    public List<Country> getAllCountries() throws JsonProcessingException {
+        ResponseEntity<String> response = restTemplate.getForEntity(restCountriesBaseUrl + "/all", String.class);
+        List<Country> countries = Arrays.asList(objectMapper.readValue(response.getBody(), Country[].class));
+        return countries;
+    }
+
+    public Country getCountryByName(String name) throws JsonProcessingException {
+        ResponseEntity<String> response = restTemplate.getForEntity(restCountriesBaseUrl + "/name/" + name, String.class);
+        JSONArray jsonArray = new JSONArray(response.getBody());
+        Country country = objectMapper.readValue(jsonArray.get(0).toString(), Country.class);
+        return country;
+    }
+
+    public String getMapsByCountryName(String country) {
+        ResponseEntity<String> response = restTemplate.getForEntity(restCountriesBaseUrl + "/name/" + country, String.class);
+        JSONArray jsonArr = new JSONArray(response.getBody());
+        JSONObject jsonObj = (JSONObject) jsonArr.get(0);
+        return (String) jsonObj.getJSONObject("maps").toMap().get("googleMaps");
+    }
+
+}
