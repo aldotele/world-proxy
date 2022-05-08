0ad50234b703ff88871d66c9b31002464e767d4b
aldotele <aldo.telese@hotmail.it>
2022-05-08 11:40:44 +0200

    uses get country english + native language

--- src/main/java/com/world/worldproxy/service/CountryServiceImpl.java
+++ src/main/java/com/world/worldproxy/service/CountryServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
