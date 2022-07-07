317bdc7995886c034905850681b903fc348126a2
aldotele <aldo.telese@hotmail.it>
2022-07-07 23:54:19 +0200

    enables web mvc

--- src/main/java/com/world/worldproxy/config/WorldProxyConfiguration.java
+++ src/main/java/com/world/worldproxy/config/WorldProxyConfiguration.java
@@ -7,9 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @Configuration
 @ComponentScan("com.world.worldproxy.service")
+@EnableWebMvc
 public class WorldProxyConfiguration {
 
     @Bean
