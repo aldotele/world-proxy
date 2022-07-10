1a7331355c9de6ce5ba916b01619648d7b20463a
aldotele <aldo.telese@hotmail.it>
2022-07-10 17:46:44 +0200

    adds flag test

--- src/test/java/com/world/worldproxy/WebTestsWithMockServer.java
+++ src/test/java/com/world/worldproxy/WebTestsWithMockServer.java
@@ -145,4 +145,24 @@ class WebTestsWithMockServer {
 		mockServer.verify();
 	}
 
+	@Test
+	public void getFlag() throws Exception {
+		// stubbing the country object of the external service
+		String stubbedExternalCountryResponse = Files.readString(Paths.get("src", "main", "resources", "stubs", "get_country_italy.json"), StandardCharsets.ISO_8859_1);
+
+		// stubbing the external request to third party API
+		mockServer.expect(ExpectedCount.once(), requestTo(new URI(restCountriesBaseUrl + "/name/italy")))
+				.andExpect(method(HttpMethod.GET))
+				.andRespond(withStatus(HttpStatus.OK)
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(stubbedExternalCountryResponse)
+				);
+
+		mockMvc.perform(get("/country/flag/italy"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.flag").value("https://flagcdn.com/it.svg"));
+
+		mockServer.verify();
+	}
+
 }
