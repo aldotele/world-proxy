6a3238943cbae8f8f612e2077487d11de0cf5fbe
aldotele <aldo.telese@hotmail.it>
2022-07-10 17:19:59 +0200

    adds all countries test

--- src/test/java/com/world/worldproxy/WebTestsWithMockServer.java
+++ src/test/java/com/world/worldproxy/WebTestsWithMockServer.java
@@ -1,10 +1,7 @@
 package com.world.worldproxy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.world.worldproxy.model.Country;
 import com.world.worldproxy.service.country.CountryService;
-import org.json.JSONArray;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +20,10 @@ import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.isA;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
@@ -66,34 +64,33 @@ class WebTestsWithMockServer {
 	public void getCountry() throws Exception {
 		// stubbing the country object of the external service
 		String stubbedExternalCountryResponse = Files.readString(Paths.get("src", "main", "resources", "stubs", "get_country_italy.json"), StandardCharsets.ISO_8859_1);
-		JSONArray stubbedExternalCountryObject = new JSONArray(stubbedExternalCountryResponse);
 
 		// stubbing the external request to third party API
 		mockServer.expect(ExpectedCount.once(), requestTo(new URI(restCountriesBaseUrl + "/name/italy")))
-				.andExpect(method(HttpMethod.GET))
-				.andRespond(withStatus(HttpStatus.OK)
-						.contentType(MediaType.APPLICATION_JSON)
-						.body(stubbedExternalCountryResponse)
-				);
+			.andExpect(method(HttpMethod.GET))
+			.andRespond(withStatus(HttpStatus.OK)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(stubbedExternalCountryResponse)
+			);
 
 		mockMvc.perform(get("/country/italy"))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.name").value("Italy"))
-				.andExpect(jsonPath("$.officialName").value("Italian Republic"))
-				.andExpect(jsonPath("$.acronym").value("ITA"))
-				.andExpect(jsonPath("$.capital").value("Rome"))
-				.andExpect(jsonPath("$.borders.length()").value(6))
-				.andExpect(jsonPath("$.maps").value( "https://goo.gl/maps/8M1K27TDj7StTRTq8"))
-				.andExpect(jsonPath("$.currencies.length()").value(1))
-				.andExpect(jsonPath("$.currencies[0]").value("Euro (€)"))
-				.andExpect(jsonPath("$.population").isNumber())
-				.andExpect(jsonPath("$.continents.length()").value(1))
-				.andExpect(jsonPath("$.continents[0]").value("Europe"))
-				.andExpect(jsonPath("$.flag").value("https://flagcdn.com/it.svg"))
-				.andExpect(jsonPath("$.languages.length()").value(1))
-				.andExpect(jsonPath("$.languages[0]").value("Italian"))
-				.andExpect(jsonPath("$.translations").isArray())
-				.andReturn();
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.name").value("Italy"))
+			.andExpect(jsonPath("$.officialName").value("Italian Republic"))
+			.andExpect(jsonPath("$.acronym").value("ITA"))
+			.andExpect(jsonPath("$.capital").value("Rome"))
+			.andExpect(jsonPath("$.borders.length()").value(6))
+			.andExpect(jsonPath("$.maps").value( "https://goo.gl/maps/8M1K27TDj7StTRTq8"))
+			.andExpect(jsonPath("$.currencies.length()").value(1))
+			.andExpect(jsonPath("$.currencies[0]").value("Euro (€)"))
+			.andExpect(jsonPath("$.population").isNumber())
+			.andExpect(jsonPath("$.continents.length()").value(1))
+			.andExpect(jsonPath("$.continents[0]").value("Europe"))
+			.andExpect(jsonPath("$.flag").value("https://flagcdn.com/it.svg"))
+			.andExpect(jsonPath("$.languages.length()").value(1))
+			.andExpect(jsonPath("$.languages[0]").value("Italian"))
+			.andExpect(jsonPath("$.translations").isArray())
+			.andReturn();
 
 		mockServer.verify();
 	}
@@ -102,22 +99,30 @@ class WebTestsWithMockServer {
 	public void getAllCountries() throws Exception {
 		// stubbing the country object of the external service
 		String stubbedExternalAllCountriesResponse = Files.readString(Paths.get("src", "main", "resources", "stubs", "get_all_countries.json"), StandardCharsets.ISO_8859_1);
-		JSONArray stubbedExternalAllCountriesObject = new JSONArray(stubbedExternalAllCountriesResponse);
-
-		// building the expected response when calling service
-		List<Country> expectedAllCountriesResponse = Arrays.asList(objectMapper.readValue(stubbedExternalAllCountriesResponse, Country[].class));
 
 		mockServer.expect(ExpectedCount.once(), requestTo(new URI(restCountriesBaseUrl + "/all")))
-				.andExpect(method(HttpMethod.GET))
-				.andRespond(withStatus(HttpStatus.OK)
-						.contentType(MediaType.APPLICATION_JSON)
-						.body(stubbedExternalAllCountriesResponse));
+			.andExpect(method(HttpMethod.GET))
+			.andRespond(withStatus(HttpStatus.OK)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(stubbedExternalAllCountriesResponse));
 
-		// storing the actual response
-		List<Country> actualAllCountriesResponse = countryService.getAllCountries();
+		mockMvc.perform(get("/country/all"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.*", isA(ArrayList.class)))
+			.andExpect(jsonPath("$.[*].name", hasItems("Italy", "Colombia", "Ghana", "China", "Australia")))
+			.andExpect(jsonPath("$.[*].capital", hasItems("Mexico City", "Moscow", "Wellington", "Beijing")))
+			.andExpect(jsonPath("$.[*].acronym", hasItems("BLM", "MCO", "NPL", "BGD")))
+			.andExpect(jsonPath("$.[0].name", isA(String.class)))
+			.andExpect(jsonPath("$.[0].capital", isA(String.class)))
+			.andExpect(jsonPath("$.[0].maps", isA(String.class)))
+			.andExpect(jsonPath("$.[0].flag", isA(String.class)))
+			.andExpect(jsonPath("$.[0].population", isA(Integer.class)))
+			.andExpect(jsonPath("$.[0].currencies", isA(ArrayList.class)))
+			.andExpect(jsonPath("$.[0].continents", isA(ArrayList.class)))
+			.andExpect(jsonPath("$.[0].translations", isA(ArrayList.class)))
+			.andReturn();
+
 		mockServer.verify();
-
-		Assertions.assertEquals(expectedAllCountriesResponse, actualAllCountriesResponse);
 	}
 
 }
