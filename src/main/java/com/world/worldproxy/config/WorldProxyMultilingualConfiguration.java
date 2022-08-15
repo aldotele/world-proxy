e1ef44eb88b8e782252e49f7468ec3a302be04da
aldotele <aldo.telese@hotmail.it>
2022-08-15 22:40:14 +0200

    uses inheritance in multilingual configuration

--- src/main/java/com/world/worldproxy/config/WorldProxyMultilingualConfiguration.java
+++ src/main/java/com/world/worldproxy/config/WorldProxyMultilingualConfiguration.java
@@ -1,14 +1,11 @@
 package com.world.worldproxy.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.world.worldproxy.service.multilingual.LanguageNormalizer;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
@@ -16,30 +13,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Profile("multilingual")
 @ComponentScan("com.world.worldproxy.service")
 @EnableWebMvc
-public class WorldProxyMultilingualConfiguration {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public CommonsRequestLoggingFilter logFilter() {
-        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
-        filter.setIncludeQueryString(true);
-        filter.setIncludePayload(true);
-        filter.setMaxPayloadLength(10000);
-        filter.setIncludeHeaders(false);
-        filter.setAfterMessagePrefix("REQUEST : ");
-        return filter;
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return objectMapper;
-    }
+@EnableAutoConfiguration
+public class WorldProxyMultilingualConfiguration extends WorldProxyConfiguration {
 
     @Bean
     public LanguageNormalizer languageConverter() {
